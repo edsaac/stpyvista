88269bf339cba04e39257e4d40ac7261f9f61b1f
Ed Saa <esaavedrac@u.northwestern.edu>
2024-09-16 08:50:16 -0500

    Add trame as an optional dependency for experimental widget

--- test/sphere.py
+++ test/sphere.py
@@ -5,13 +5,13 @@ import pyvista as pv
 from stpyvista import experimental_vtkjs
 from stpyvista.export import export_vtksz
 
+
 @st.cache_resource
-def create_plotter(dummy:str = "sphere"):
-    
+def create_plotter(dummy: str = "sphere"):
     # Initialize a plotter object
-    plotter = pv.Plotter(shape=(2,1))
+    plotter = pv.Plotter()
     mesh = pv.Sphere(radius=1.0, center=(0, 0, 0))
-    
+
     x, y, z = mesh.cell_centers().points.T
     mesh["My scalar"] = z
 
@@ -23,34 +23,40 @@ def create_plotter(dummy:str = "sphere"):
         show_edges=True,
         edge_color="#001100",
         ambient=0.2,
-        show_scalar_bar = False
+        show_scalar_bar=False,
     )
-        
+
     ## Some final touches
     plotter.view_isometric()
     plotter.background_color = "pink"
-    
+
     return plotter
 
-async def main():
 
+async def main():
     st.set_page_config(page_icon="🧊", layout="wide")
     st.title("🧊 `stpyvista`")
     st.write("*Show PyVista 3D visualizations in Streamlit*")
-    
+
     plotter = create_plotter()
 
     if "data" not in st.session_state:
         st.session_state.data = await export_vtksz(plotter)
-    
-    lcol, rcol = st.columns(2)
-    with rcol:
-        "🌎 3D Model"
-        camera = experimental_vtkjs(st.session_state.data, key="experimental-stpv")
-        
+
+    lcol, rcol = st.columns([2, 1])
     with lcol:
+        with st.form("experimental_vtkjs"):
+            "🌎 3D Model"
+            camera = experimental_vtkjs(st.session_state.data, key="experimental-stpv")
+            grab_camera = st.form_submit_button(":camera: Grab view state")
+
+    with rcol:
         "🎥 Camera"
-        st.json(camera)
+        if grab_camera:
+            st.json(camera)
+        else:
+            st.info("Click the button to grab the view state")
+
 
 if __name__ == "__main__":
     asyncio.run(main())

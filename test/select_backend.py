769757481bb13233e96797e164fb1ed474a3425e
Ed Saa <esaavedrac@u.northwestern.edu>
2024-10-01 15:50:49 -0500

    Add example for select backend

--- test/select_backend.py
+++ test/select_backend.py
@@ -1,37 +1,43 @@
 import pyvista as pv
 import streamlit as st
-
-backend = st.selectbox("Backend", ["panel", "trame"])
-
-if backend == "panel":
-    from stpyvista.panel_backend import stpyvista
-elif backend == "trame":
-    from stpyvista.trame_backend import stpyvista
-
+from stpyvista.panel_backend import stpyvista as stpv_panel
+from stpyvista.trame_backend import stpyvista as stpv_trame
 
 st.title("🧊 `stpyvista`")
-st.header("Show PyVista 3D visualizations in Streamlit")
+st.subheader("Show PyVista 3D visualizations in Streamlit")
 
 ## Initialize a plotter object
-plotter = pv.Plotter(window_size=[200, 200])
+plotter = pv.Plotter(window_size=[350, 350])
 
-## Create a mesh with a cube
-mesh = pv.Cube(center=(0, 0, 0))
+## Create a mesh with a sphere
+mesh = pv.Sphere(radius=1.0, center=(0, 0, 0))
 
 ## Add some scalar field associated to the mesh
-mesh["myscalar"] = mesh.points[:, 2] * mesh.points[:, 0]
-mesh["otherscalar"] = mesh.points[:, 2] ** 2 + mesh.points[:, 0]
-mesh.set_active_scalars("otherscalar")
+x, y, z = mesh.cell_centers().points.T
+mesh["My scalar"] = z
+mesh.set_active_scalars("My scalar")
 
 ## Add mesh to the plotter
-plotter.add_mesh(mesh, cmap="bwr", line_width=1, label="cube")
-plotter.add_title("My cube", color="black", font_size=14)
+plotter.add_mesh(
+    mesh,
+    scalars="My scalar",
+    cmap="prism",
+    show_edges=True,
+    edge_color="#001100",
+    ambient=0.2,
+    show_scalar_bar=True,
+)
 
 ## Final touches
+plotter.add_title("My Sphere", color="black", font_size=14)
 plotter.view_isometric()
-plotter.background_color = "red"
+plotter.background_color = "#dddddd"
 
-stpyvista(plotter, key="my_plot")
+cols = st.columns(2, vertical_alignment="center")
+with cols[0]:
+    st.write("**Using Panel (default)**")
+    stpv_panel(plotter, key="stpv_panel")
 
-if st.checkbox("Do something"):
-    st.write("Hello")
+with cols[1]:
+    st.write("**Using Trame**")
+    stpv_trame(plotter, key="stpv_trame")
